ba14cd74031a91495dbb167f1320832d8615eff4
Posakaveri28 <srinivasuluposa4@gmail.com>
2026-02-16 10:33:23 +0530

    Time: 10 ms (9.57%) | Memory: 28.7 MB (80.64%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int a,b,c;
+        int mindiff=INT_MAX;
+       for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            for(int k=j+1;k<nums.size();k++){
+                if(nums[i]==nums[j] && nums[j]==nums[k]){
+                    int dist = abs(i-j) + abs(j-k) + abs(k-i);
+                        mindiff = min(mindiff, dist);
+                }
+            }
+        }
+        
+       }
+       if(mindiff == INT_MAX)
+            return -1; 
+        return mindiff; 
+    }
+};
